99a6baa3f3114f449be1184a6be6c88a42744c75
Abdullahi Mohamed Mohamoud <117075003+AbdullahiMo938@users.noreply.github.com>
2025-12-12 22:10:38 +0000

    Integration Test

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,8 @@
 import request from 'supertest';
 import app from '../index.js';
+import { jest } from '@jest/globals';
+global.fetch = jest.fn();
+
 test('version check' , async () => {
 const response =  await request(app).get('/version');
 expect(response.text).toContain("API running");
@@ -7,3 +10,43 @@ expect(response.text).toContain("API running");
 test('1 + 1 equals 2', () => {
   expect(1 + 1).toBe(2);
 });
+
+const fakeData = [
+  { 
+    createdAt: new Date().toISOString(), // Right now (Recent)
+    value: '10' 
+  },
+  { 
+    createdAt: '2000-01-01T00:00:00Z',   // Ancient history (Old)
+    value: '100' 
+  }
+];
+
+// fetch.mockResolvedValue({
+//   json: () => Promise.resolve(fakeData)
+// });
+
+test('GET /temperature calculates average', async () => {
+
+  // 1. ARRANGE: Tell fetch what to return
+
+  fetch.mockResolvedValue({
+
+    json: () => Promise.resolve(fakeData)
+
+  });
+
+
+
+  // 2. ACT: Ask the app for the page (MISSING!)
+
+  const response = await request(app).get('/temperature')
+
+
+
+  // 3. ASSERT: Check the answer
+
+  expect(response.text).toContain("10");
+
+});
+
