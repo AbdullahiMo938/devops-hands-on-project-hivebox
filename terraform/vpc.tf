002374352c6c77fbb0a6a814877528627b42bc4f
Abdullahi Mohamed Mohamoud <117075003+AbdullahiMo938@users.noreply.github.com>
2026-05-04 18:08:17 +0100

    add CI/CD workflows for application and Terraform infrastructure

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,19 +7,19 @@ module "vpc" {
 
   azs = ["eu-west-2a", "eu-west-2b"]
 
-  # Public subnets ONLY (simplest EKS setup)
+  
   public_subnets = [
     "10.0.101.0/24",
     "10.0.102.0/24"
   ]
 
-  # IMPORTANT: nodes need internet access without NAT
+  
   enable_nat_gateway = false
 
   # Required for EKS nodes to get public IPs
   map_public_ip_on_launch = true
 
-  # EKS tags (IMPORTANT)
+
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
     "kubernetes.io/cluster/hivebox-london-cluster" = "shared"
